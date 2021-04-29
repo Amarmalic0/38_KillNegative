c177ca22eff899f092ec19915093c1a31cf6d3c0
Amarmalic0 <81154113+Amarmalic0@users.noreply.github.com>
2021-04-29 21:50:36 +0200

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] zahlen = {10};
+        zahlen = new int[10];
+
+        if (zahlen < 0) {
+            System.out.println("Die eingabe wird durch 0 ersetzt;");
+        }
+        static int zahlen ( int[] zahlen){
+            return zahlen[zahlen.length - 1];
+
+
+        }
+    }
+}
